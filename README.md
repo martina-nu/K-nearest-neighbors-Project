1e801a8e33682454fd0629682bf8060de69dd775
martina-nu <martinanu98@gmail.com>
2022-08-04 14:45:39 +0000

    Add solution

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Template for Machine Learning projects
+# K-nearest neighbors Project Tutorial
+
+
+**In this guided project we will learn how to build a simple movie recommender system using k-nearest neighbors algorithm.**
+
+Can we predict which films will be highly rated, even if they are not a commercial success?
+
+This dataset is a subset of the huge TMDB Movie Database API, containing only 5000 movies from the total number.
--- src/app.py
+++ src/app.py
@@ -1,4 +1,106 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# your code here
+
+#Import data
+
+movies = pd.read_csv('data/raw/tmdb_5000_movies.csv')
+credits = pd.read_csv('data/raw/tmdb_5000_credits.csv')
+
+
+#Merge data
+movies = movies.merge(credits, on='title')
+
+#Select columns
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+
+#Drop na
+movies.dropna(inplace = True)
+
+#Convert columns in json format
+
+import ast
+
+def convert(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        L.append(i['name'])
+    return L
+
+
+movies['genres'] = movies['genres'].apply(convert)
+
+movies['keywords'] = movies['keywords'].apply(convert)
+
+def convert3(obj):
+    L = []
+    count = 0
+    for i in ast.literal_eval(obj):
+        if count < 3:
+            L.append(i['name'])
+        count +=1  
+    return L
+
+movies['cast'] = movies['cast'].apply(convert3)
+
+def fetch_director(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        if i['job'] == 'Director':
+            L.append(i['name'])
+            break
+    return L
+
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+movies['overview'] = movies['overview'].apply(lambda x : x.split())
+
+def collapse(L):
+    L1 = []
+    for i in L:
+        L1.append(i.replace(" ",""))
+    return L1
+
+movies['cast'] = movies['cast'].apply(collapse)
+movies['crew'] = movies['crew'].apply(collapse)
+movies['genres'] = movies['genres'].apply(collapse)
+movies['keywords'] = movies['keywords'].apply(collapse)
+
+#Reduce df
+
+movies['tags'] = movies['overview']+movies['genres']+movies['keywords']+movies['cast']+movies['crew']
+
+new_df = movies[['movie_id','title','tags']]
+
+new_df['tags'] = new_df['tags'].apply(lambda x :" ".join(x))
+
+#KNN
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=5000 ,stop_words='english')
+
+vectors = cv.fit_transform(new_df['tags']).toarray()
+
+#Find cosine similarity
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+similarity = cosine_similarity(vectors)
+
+sorted(list(enumerate(similarity[0])),reverse =True , key = lambda x:x[1])[1:6]
+
+#Recommendation function
+
+def recommend(movie):
+    movie_index = new_df[new_df['title'] == movie].index[0] ##fetching the movie index
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate( distances)),reverse =True , key = lambda x:x[1])[1:6]
+    
+    for i in movie_list:
+        print(new_df.iloc[i[0]].title)
+
+#Verify that works
+print(recommend('Shrek'))
+
